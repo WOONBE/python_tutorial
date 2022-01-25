34aa1c66427162ebe9f6ea30a744d1aa63d1a68d
WOONBE <kepull2918@naver.com>
2022-01-25 23:03:53 +0900

    0125

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 print("재수")
 
 print("잘자" in "잘자세요")
+
+numbers = [1,2,6,8,5,3,2,1,9,7,2,3]
+counter = {}
+
+for number in numbers:
+    if number in counter:
+        counter[number] = counter[number] + 1
+    else:
+        counter[number] = 1
+print(counter)
