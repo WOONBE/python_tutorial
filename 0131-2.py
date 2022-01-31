7c9be603dedb75c330b6ec25ecbc734715fbcd79
WOONBE <kepull2918@naver.com>
2022-01-31 14:27:01 +0900

    0131-2

---
+++ 0131-2.py
@@ -0,0 +1,16 @@
+class Student:
+    def __str__(self):
+        return "{} {}". format(self.이름, self.나이)
+    def __init__(self, 이름, 나이):
+        print("객체가 생성되었습니다")
+        self.이름 = 이름
+        self.나이 = 나이
+    def __del__(self):
+        print("객체가 소멸되었습니다.")
+    def 출력(self):  
+        print(self.이름, self.나이)
+    
+student = Student("윤인성", 3)
+student.출력()
+print(str(student))
+
