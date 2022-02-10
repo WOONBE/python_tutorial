477dc0d3b47b219a2654d56a42b17dec9968784f
WOONBE <kepull2918@naver.com>
2022-02-10 12:59:11 +0900

    0210-2

---
+++ Gui기본/8_combobox.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480")
+
+combobox
+
+
+btn_drink1.pack()
+btn_drink2.pack()
+
+
+def btncmd():
+    pass
+btn = Button(root, text="주문", command=btncmd)
+btn.pack()
+
+
+root.mainloop()
