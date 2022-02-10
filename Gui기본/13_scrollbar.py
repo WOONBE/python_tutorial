110337f82f882bb86d1f5b2b1fb6c5304c3a8283
WOONBE <kepull2918@naver.com>
2022-02-10 17:18:48 +0900

    0210-7

--- Gui기본/13_scrollbar.py
+++ Gui기본/13_scrollbar.py
@@ -7,10 +7,16 @@ root.geometry("640x480")
 frame = Frame(root)
 frame.pack()
 
-listbox = Listbox(frame, selectmode="extended", height=10)
+scrollbar = Scrollbar(frame)
+scrollbar.pack(side="right", fill="y")
+
+# set이 없으면 스크롤을 내려도 다시 올라옴
+listbox = Listbox(frame, selectmode="extended", height=10, yscrollcommand = scrollbar.set)
 for i in range(1, 32):
     listbox.insert(END, str(i) + "일")
-listbox.pack()  
+listbox.pack(side="left") 
+
+scrollbar.config(command=listbox.yview)
 
 
 
