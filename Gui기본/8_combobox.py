531bcd25eb73162fd071e03382d820e1cf3f6e89
WOONBE <kepull2918@naver.com>
2022-02-10 14:14:46 +0900

    0210-4

--- Gui기본/8_combobox.py
+++ Gui기본/8_combobox.py
@@ -1,19 +1,25 @@
+import tkinter.ttk as ttk
 from tkinter import *
 
 root = Tk()
 root.title("Nado GUI")
 root.geometry("640x480")
 
-combobox
+values = [str(i) + "일" for i in range(1, 32)] # 1~31까지의 숫자
+combobox = ttk.Combobox(root, height=5, values=values)
+combobox.pack()
+combobox.set("카드 결제일") #최초 목록 제목 설정
 
+readonly_combobox = ttk.Combobox(root, height=10, values=values, state="readonly") #읽기 전용으로 변환
+readonly_combobox.current(0) #0번째 인덱스 값 선택
+readonly_combobox.pack()
 
-btn_drink1.pack()
-btn_drink2.pack()
 
 
 def btncmd():
-    pass
-btn = Button(root, text="주문", command=btncmd)
+    print(combobox.get()) # 선택된 값 표시
+
+btn = Button(root, text="선택", command=btncmd)
 btn.pack()
 
 
