ecc7da333474b076ef1083495fbf5ad072e95322
WOONBE <kepull2918@naver.com>
2022-02-10 12:29:58 +0900

    0210-1

---
+++ Gui기본/7_radiobutton.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480")
+
+
+
+
+def btncmd():
+    pass
+btn = Button(root, text="클릭", command=btncmd)
+btn.pack()
+
+
+root.mainloop()
